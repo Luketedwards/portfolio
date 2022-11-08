212088863a2fd5244fa3257e28425a59ccd081a2
Luketedwards <Luketedwards96@hotmail.com>
2022-11-08 15:27:21 +0000

    added environ

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -165,7 +165,7 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
