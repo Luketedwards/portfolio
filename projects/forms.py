a121d93f9b950b9eb782891dab6faddeba8557e1
Luketedwards <Luketedwards96@hotmail.com>
2022-11-11 13:39:00 +0000

    added new js files

--- projects/forms.py
+++ projects/forms.py
@@ -6,7 +6,7 @@ import datetime
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ('title', 'description', 'image', 'image2','image3','image4','url', 'category', 'date', 'type')
+        fields = ('title', 'description', 'image', 'image2','image3','image4','url', 'category', 'date', 'type', 'skills')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
